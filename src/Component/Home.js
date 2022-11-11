68db1578836d5cd6f2f48fcf6186180db04259ac
SJain0311 <simranjain031196@gmail.com>
2022-11-11 18:50:02 +0530

    solve issue

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -85,10 +85,9 @@ function Home() {
                     data-bs-ride="carousel"
                   >
                     <div className="carousel-inner">
-                      <div className="carousel-item active">
+                      <div className="carousel-item active ">
                         <div className="row">
-                          <div className="col-1"></div>
-                          <div className="col-3">
+                          <div className="col-sm-4">
                             <div className="" id="card1">
                               <img
                                 className="card-img-top"
@@ -105,8 +104,8 @@ function Home() {
                               </div>
                             </div>
                           </div>
-                          <div className="col-1"></div>
-                          <div className="col-3">
+
+                          <div className="col-sm-4">
                             <div className="" id="card1">
                               <img
                                 className="card-img-top"
@@ -117,14 +116,14 @@ function Home() {
                               <div className="card-body">
                                 <h5 className="card-title">Printers</h5>
                                 <p className="card-text">From ₹3999</p>
-                                <p className="" style={{ color: "black" }}>
-                                  HP
-                                </p>
+                                <span>HP</span>
+                                {/* <a href="#" className="btn btn-primary">
+                              Buy
+                            </a> */}
                               </div>
                             </div>
                           </div>
-                          <div className="col-1"></div>
-                          <div className="col-3">
+                          <div className="col-sm-4">
                             <div className="" id="card1">
                               <img
                                 className="card-img-top"
@@ -143,8 +142,7 @@ function Home() {
                       </div>
                       <div className="carousel-item ">
                         <div className="row">
-                          <div className="col-1"></div>
-                          <div className="col-3">
+                          <div className="col-sm-4">
                             <div className="" id="card1">
                               <img
                                 className="card-img-top"
@@ -159,11 +157,10 @@ function Home() {
                               </div>
                             </div>
                           </div>
-                          <div className="col-1"></div>
-                          <div className="col-3">
+                          <div className="col-sm-4">
                             <div className="" id="card1">
                               <img
-                                className=""
+                                className="card-img-top"
                                 src="https://rukminim1.flixcart.com/image/200/200/hair-dryer/c/t/g/philips-hp8100-46-original-imaemymzuxrnzfjb.jpeg?q=70"
                                 alt="Card image cap"
                                 id="g_images"
@@ -177,11 +174,10 @@ function Home() {
                               </div>
                             </div>
                           </div>
-                          <div className="col-1"></div>
-                          <div className="col-3">
+                          <div className="col-sm-4">
                             <div className="" id="card1">
                               <img
-                                className=""
+                                className="card-img-top"
                                 src="https://rukminim1.flixcart.com/image/200/200/ko8xtow0/monitor/t/a/y/d24-20-66aekac1in-lenovo-original-imag2qwzazcdmqtb.jpeg?q=70"
                                 alt="Card image cap"
                                 id="g_images"
@@ -197,11 +193,10 @@ function Home() {
                       </div>
                       <div className="carousel-item ">
                         <div className="row">
-                          <div className="col-1"></div>
-                          <div className="col-3">
-                            <div className="card" id="card1">
+                          <div className="col-sm-4">
+                            <div className="" id="card1">
                               <img
-                                className=""
+                                className="card-img-top"
                                 src="https://rukminim1.flixcart.com/image/200/200/kmp7ngw0/monitor/j/z/h/s2721hn-27-py0df-dell-original-imagfjphuywuh2a7.jpeg?q=70"
                                 alt="Card image cap"
                                 id="g_images"
@@ -213,12 +208,10 @@ function Home() {
                               </div>
                             </div>
                           </div>
-                          <div className="col-1"></div>
-                          <div className="col-3">
+                          <div className="col-sm-4">
                             <div className="" id="card1">
                               <img
-                                className=""
-                                style={{width:200}}
+                                className="card-img-top"
                                 src="https://rukminim1.flixcart.com/image/200/200/l1pc3gw0/power-bank/e/u/y/-original-imagd7dzan7sakt2.jpeg?q=70"
                                 alt="Card image cap"
                                 id="g_images"
@@ -232,11 +225,10 @@ function Home() {
                               </div>
                             </div>
                           </div>
-                          <div className="col-1"></div>
-                          <div className="col-3">
+                          <div className="col-sm-4">
                             <div className="" id="card1">
                               <img
-                                className=""
+                                className="card-img-top"
                                 src="https://rukminim1.flixcart.com/image/400/400/kxxl9jk0/monitor/r/l/e/rog-strix-xg32vc-wqhd-31-5-90lm03s0-b04110-asus-original-imaga9ubzrvh4spj.jpeg?q=70"
                                 alt="Card image cap"
                                 id="g_images"
@@ -322,7 +314,9 @@ function Home() {
                                 id="g_images"
                               />
                               <div className="card-body">
-                                <h5 className="card-title">Wall decor Item</h5>
+                                <h5 className="card-title">
+                                 Wall decor Item
+                                </h5>
                                 <p className="card-text">Shop Now</p>
                                 <span> Big DisCounts</span>
                               </div>
@@ -330,7 +324,7 @@ function Home() {
                           </div>
 
                           <div className="col-sm-4">
-                            <div className="" id="card1">
+                            <div className="card" id="card1">
                               <img
                                 className="card-img-top"
                                 src="https://rukminim1.flixcart.com/image/400/400/kirr24w0-0/wall-decoration/f/z/b/rafuf-wooden-intersecting-wall-shelves-set-of-8-black-white-8-original-imafyhg9dzdvyhnz.jpeg?q=70"
@@ -356,7 +350,9 @@ function Home() {
                                 id="g_images"
                               />
                               <div className="card-body">
-                                <h5 className="card-title">Wallpapers</h5>
+                                <h5 className="card-title">
+                                  Wallpapers
+                                </h5>
                                 <p className="card-text">From ₹399</p>
                                 <span>Big Discounts</span>
                               </div>
@@ -390,14 +386,16 @@ function Home() {
                                 id="g_images"
                               />
                               <div className="card-body">
-                                <h5 className="card-title">Wallpaper</h5>
+                                <h5 className="card-title">
+                                  Wallpaper
+                                </h5>
                                 <p className="card-text">From 399</p>
                                 <span>Big DisCount</span>
                               </div>
                             </div>
                           </div>
                           <div className="col-sm-4">
-                            <div className="" id="card1">
+                            <div className="card" id="card1">
                               <img
                                 className="card-img-top"
                                 src="https://rukminim1.flixcart.com/image/400/400/xif0q/welding-machine/c/7/3/15-m220-76-arc-igbt-220a-with-hot-start-anti-stick-functions-arc-original-imaggaxpxcs4ffwg.jpeg?q=70"
@@ -407,7 +405,7 @@ function Home() {
                               <div className="card-body">
                                 <h5 className="card-title">Monitors</h5>
                                 <p className="card-text">From ₹7949</p>
-                                <p className="card-text">From ₹7949</p>
+                                <span>BIg Discounts</span>
                               </div>
                             </div>
                           </div>
@@ -416,7 +414,7 @@ function Home() {
                       <div className="carousel-item ">
                         <div className="row">
                           <div className="col-sm-4">
-                            <div className="" id="card1">
+                            <div className="card" id="card1">
                               <img
                                 className="card-img-top"
                                 src="https://rukminim1.flixcart.com/image/200/200/kmp7ngw0/monitor/j/z/h/s2721hn-27-py0df-dell-original-imagfjphuywuh2a7.jpeg?q=70"
@@ -431,7 +429,7 @@ function Home() {
                             </div>
                           </div>
                           <div className="col-sm-4">
-                            <div className="" id="card1">
+                            <div className="card" id="card1">
                               <img
                                 className="card-img-top"
                                 src="https://rukminim1.flixcart.com/image/200/200/l1pc3gw0/power-bank/e/u/y/-original-imagd7dzan7sakt2.jpeg?q=70"
